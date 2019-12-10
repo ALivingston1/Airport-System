9063ede74aee3727617977658b7eb6664fc4ffca
adelmonico <adelmonico@techvalleyhigh.org>
2019-12-10 09:16:39 -0500

    Take this

---
+++ src/Passenger.java
@@ -0,0 +1,34 @@
+public class Passenger{
+  String name;
+  String seatType;
+  int luggage;
+  
+  
+  
+  
+  public Passenger(String name2, String seatType2, int luggage2){
+    name = name2;
+    seatType = seatType2;
+    luggage = luggage2;
+  }
+  
+  
+  public String getName(){
+  return name;
+  }
+  
+  
+  public String getSeatType(){
+    return seatType;
+  }
+  
+  public int getLuggage(){
+    return luggage;
+  }
+  
+  
+  
+}
+    
+    
+   
